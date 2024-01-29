cef12a949612d4cdaf3591156f38168d5d0fac28
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-01-29 13:01:27 +0100

    interface to add robots to a list ok

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMauiApp
+{
+    internal class Robot
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Size { get; set; }
+        public Robot(string name, string type, int size)
+        {
+            Name = name;
+            Type = type;
+            Size = size;
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,42 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
+        private void createRobot(object sender, EventArgs e)
+        {
+            string name = nameEntry.Text; 
+            string type = (string)typePicker.SelectedItem;
+            int size = (int)Math.Round(slider.Value);
 
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
+            if (!string.IsNullOrWhiteSpace(name) && type != null && size > 0)
+            {
+                Robot newRobot = new Robot(name, type, size);
 
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
+                Label robotLabel = new Label
+                {
+                    Text = $"Nouveau robot nom: {newRobot.Name} \nType de Robot: {newRobot.Type} \nTaille de Robot: {newRobot.Size}",
+                    HorizontalOptions = LayoutOptions.Center,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)), // Set a predefined size for the font
+                    Margin = 10,
+                };
+
+                robotsStackLayout.Children.Add(robotLabel);
+
+                // Optionally clear the inputs
+                nameEntry.Text = string.Empty;
+                typePicker.SelectedItem = null;
+                slider.Value = 0;
+            }
+            else
+            {
+                DisplayAlert("Information manquante", "Veuillez renseigner le nom, le type et la taille du robot.", "OK");
+            }
+        }
     }
 }
